91a0f217a428ec75b139d0ea4aab2eb10e15a626
GUOHAO14 <guohao.k@hotmail.com>
2025-12-18 18:36:05 +0800

    hello

--- src/project/utils/InteractTxt.java
+++ src/project/utils/InteractTxt.java
@@ -7,7 +7,7 @@ import project.roles.Class;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
-import haha;
+import hehehe;
 
 public class InteractTxt {
     public static ArrayList<Student> allStudent = new ArrayList<Student>();
